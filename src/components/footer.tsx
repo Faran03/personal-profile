54c9d01bd24281f42cfca0c16f85173fae818e31
Faran03 <faranahmadf@gmail.com>
2025-10-08 15:46:37 +0700

    Add Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <div className="w-[100%] h-[7vh] font-light text-[16px]/[18px] bg-[#292F36] flex justify-between items-center px-16">
+      <div>2025 FaranAja. All Right Reserved.</div>
+      <div className="hover:text-[#12F7D6] hover:cursor-pointer">
+        Privacy Policy
+      </div>
+      <div className="hover:text-[#12F7D6] hover:cursor-pointer">
+        Terms & Conditions
+      </div>
+      <div>Design by JohannLeon</div>
+    </div>
+  );
+}
